594f53f7a330d653f53f9c0e22d0640dfc96e048
Ou Shiwei <shiwei.ou@protonmail.com>
2022-03-25 00:47:27 +0800

    First Commit

---
+++ hello-world.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("It's before noon")
+	default:
+		fmt.Println("It's after noon")
+	}
+}
